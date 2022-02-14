b85f037cc54d7e0766c3ed90dc4e21ecf752d35a
Alexander Leino <Alexander.Leino@yahoo.com>
2022-02-14 18:01:50 -0500

    created homepage

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -9,7 +9,8 @@ import business from '../images/business.jpg'
 import {FaJava} from 'react-icons/fa'
 import { Box } from '@mui/material';
 
-export default function FeatureGroupCard() {
+export default function FeatureGroupCard({name, techNeeded, about}) {
+  console.log(name, techNeeded, about)
   return (
     <Card sx={{ maxWidth: 345 }} style={{borderRadius:'25px', boxShadow: "12px 12px 2px 1px rgba(0, 0, 255, .2)", border:'1px solid black'}}>
       <CardMedia
@@ -21,20 +22,24 @@ export default function FeatureGroupCard() {
       />
       <CardContent style={{padding:'1rem'}}>
         <Typography gutterBottom variant="h5" component="div" >
-          Name of Group
+          {name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          This is the coolest group you will ever get involved with.
+
+        <Typography style={{textAlign:'center', marginBottom:'.5rem'}} variant="body2" color="text.secondary">
+          {about}
         </Typography>
+
         <Typography color="text.primary">
           Technologies Needed: 
         </Typography>
-        <Box mt={1} style={{display:'flex', flexWrap:'wrap'}}>
-          <FaJava style={{fontSize:'30px', color:'#3A205C'}}/>
+        
+        <Box mt={1} style={{display:'flex', flexWrap:'wrap', justifyContent:'space-around', fontSize:'30px'}}>
+          {techNeeded}
         </Box>
+
       </CardContent>
       <CardActions style={{display: 'flex',justifyContent: 'center'}}>
-        <Button size="small" variant='contained' style={{background:'#3A205C', fontWeight:'bold', marginBottom:'.25rem'}}>Learn More</Button>
+        <Button size="small" variant='contained' style={{background:'#3A205C', fontWeight:'bold', marginBottom:'.25rem', padding:'.35rem'}}>Learn More</Button>
       </CardActions>
     </Card>
   );
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,41 +1,81 @@
 import React from 'react'
 import './HomePage.css'
 import Auth from '../utils/auth'
-import { Button, Typography } from '@mui/material'
-import gradient from '../dreamy gradients Γÿü∩╕Å Γ£¿/strawberrylemonade.png'
-import sourceFinder from '../images/sourceFinder.svg'
-import Grid  from '@mui/material/Grid'
+import {Typography } from '@mui/material'
 import  Box  from '@mui/material/Box'
 import Card from '../components/Card'
 import Container from '@mui/material/Container'
-import { height } from '@mui/system'
+import groupImage from '../images/teamwork.svg'
+import {IoLogoJavascript} from 'react-icons/io'
+import { SiMongodb, SiChakraui, SiPython, SiJquery, SiFigma, SiReact, SiBootstrap} from 'react-icons/si'
+import { GrGraphQl, GrMysql } from 'react-icons/gr'
 
-
-
-
+const groupData = [
+  {   
+     
+      groupName: 'Source Finder',
+      techNeeded: [<IoLogoJavascript/>, <SiMongodb/>, <SiFigma/>, <SiBootstrap/>],
+      aboutGroup: 'This group is pretty new and we are looking for a lot of different personal please apply within.',
+      category: 'Web Development',
+      adminId: '1',
+      
+  },
+  {   
+     
+      groupName: 'Freelancers R Us',
+      techNeeded: [<GrGraphQl/>, <SiChakraui/>, <SiReact/>],
+      aboutGroup: 'This group is all about finding the best artists in LA.',
+      category: 'Web Development',
+      adminId: '2',
+  },
+  {
+     
+      groupName: 'Football Fanatics',
+      techNeeded: [<SiPython/>, <GrMysql/>, <SiJquery/>],
+      aboutGroup: 'Looking for people that are passionate about footaball.',
+      category: 'Web Development',
+      adminId: '3',
+  }
+]
 
 
 function HomePage() {
   const userData = Auth.getProfile()
+  
+
   return (
-  <>
-  <Container maxWidth='xl' style={{display:'flex', justifyContent:'center', marginTop:'1.25rem'}}>
-    <Typography variant="h4" style={{}}>Featured Groups</Typography>
+    <>
+  <Box className='spacer layer1' style={{paddingBottom:'10px'}}>
+  <Container maxWidth='xl' style={{display:'flex', justifyContent:'center', paddingTop:'1.5rem'}}>
+    <Typography variant="h3" style={{textShadow:"1px 1px 2px pink", color:'white', fontWeight:'bold', textAlign:'center'}}>Featured Groups</Typography>
   </Container>
-  <Box style={{display:'flex', justifyContent:'center', flexWrap:'wrap'}}>
-    <Box margin={3}>
-      <Card />
-    </Box>
-    <Box margin={3}>
-      <Card />
-    </Box>
-    <Box margin={3}>
-      <Card />
-    </Box>
+ 
+<Box style={{display:'flex', justifyContent:'center', flexWrap:'wrap', marginBottom:'5rem'}}>
+    {groupData.map(group => {
+      return <Box margin={3}><Card name={group.groupName} techNeeded={group.techNeeded} about={group.aboutGroup}/>
+ </Box>
+    })}
+  
   </Box>
-  <Box mt={5}> 
-    <h1 style={{textAlign:'center'}}>Who Are We?</h1>
+  <Box mt={30}>
+    
   </Box>
+
+  <Container maxWidth='100%' style={{display:'flex', justifyContent:'space-between', flexWrap:'Wrap'}}>
+    <Box style={{width:'50%'}}>
+      <Typography variant='h1' style={{color:'white', textAlign:'center', fontWeight:'bold', textShadow:"1px 1px 2px pink",}}>Who Are We?</Typography>
+      
+        <Typography variant='h5' style={{ color:'white', textAlign:'center', marginTop:'50px', fontWeight:'bold', lineHeight:'3.5rem'}}>Our Mission is to create a fun and accessible enviroment where computer programmers can connect with one another and build something truly great! </Typography>
+      
+    </Box>
+      <img src={groupImage}/>
+  </Container>
+  <Box mt={10}>
+  <Typography variant='h3' style={{color:'white', textAlign:'center', fontWeight:'bold', textShadow:"1px 1px 2px pink", width:'100%'}}>How Do You Get Involved?</Typography>
+  </Box>
+  
+  </Box>
+ 
   </>
   )
 }
