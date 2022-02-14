7592d23084854ec5fe7a61c8db29d5e7e1cf99de
Quentin Hnilica <QuentinHnilica@gmail.com>
2022-02-14 16:45:37 -0500

    Made a new create group form where you can upload a group profile pic

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 //import SidebarCategories from './components/SidebarCategories'
 // import QHome from './pages/QHome'
+import Auth from './utils/auth'
 import Login from './components/Login'
 import EditProfile from './components/EditProfile'
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import QMakeGroup from "./components/QMakeGroup";
 import GroupPage from "./pages/GroupPage";
+import NavBar from './components/NavBar'
 
 
 const client = new ApolloClient({
@@ -18,9 +20,11 @@ const client = new ApolloClient({
 });
 
 function App() {
+  const userData = Auth.getProfile()
   return (
     <ApolloProvider client={client}>
     <Router>
+      <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/login' element={<Login />}></Route>
