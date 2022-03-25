e9cd04d0ceff942724c77df93073c29fbfce56a7
Alexander Leino <Alexander.Leino@yahoo.com>
2022-03-25 00:59:45 -0400

    switch statement works

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,19 +1,43 @@
 import { Html } from "@mui/icons-material"
 import { IoLogoJavascript } from 'react-icons/io'
-import {FaJava} from 'react-icons/fa'
+import {FaJava, FaCss3Alt, FaReact, FaAngular, FaHtml5} from 'react-icons/fa'
+import { GrMysql } from 'react-icons/gr'
+import {SiMongodb, SiJquery, SiSequelize, SiFigma} from 'react-icons/si'
 
 export default function helperFunctions() {
     function getIcon(string) {
         
         switch(string){
-            case "HTML":
-                console.log("it worked")
-                return <Html />
+            case "HTML":  
+                return <FaHtml5 />
             case "JavaScript":
-                console.log('JavaScript')
                 return <IoLogoJavascript/> 
             case "Java":
                 return <FaJava />
+            case "CSS5":
+                return <FaCss3Alt />
+            case "Mysql":
+                return <GrMysql />
+            case "React":
+                return <FaReact />
+            case "Angular":
+                return <FaAngular />
+            case "Mongoose":
+                return "Mongoose"
+            case "Mongodb":
+                return <SiMongodb />
+            case "jQuery":
+                return <SiJquery/>
+            case "Sequelize":
+                return <SiSequelize />
+            case "Unity":
+                return <SiUnity />
+            case "C++":
+                return "C++"
+            case "Java":
+                return <FaJava />
+            case "Figma":
+                return <SiFigma/>
         }
     }
     return { getIcon }
